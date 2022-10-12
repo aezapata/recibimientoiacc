d1a42677fcb30150170c74862fe822faa4fee896
Alexis Zapata <alexiszapata@CielosMcbookBro.fibertel.com.ar>
2022-10-12 14:04:58 -0300

    fourth commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,19 @@
 import type { NextPage } from "next";
-import Head from "next/head";
 import Layout from "../shared/layout";
 import styles from "../styles/Home.module.css";
-
+import Image from 'next/image'
 const Home: NextPage = () => {
   return (
     <Layout>
       <main className={styles.main}>
+      <Image
+                src="/favicon.ico"
+                alt="IACC"
+                priority={true}
+                width={250}
+                height={250}
+                layout="fixed"
+              />
         <h1 className={styles.title}>Será por defender estos colores</h1>
       </main>
     </Layout>
--- shared/components/header.tsx
+++ shared/components/header.tsx
@@ -5,14 +5,7 @@ export const Header = () => {
   const {isMobile} = useDeviceDetect()
   return (
     <div className={styles.container}>
-       <Image
-                src="/favicon.ico"
-                alt="IACC"
-                priority={true}
-                width={isMobile ? 250 : 94.2}
-                height={isMobile ? 250 : 69.6}
-                layout="fixed"
-              />
+     
       <div> </div>
     </div>
   );
