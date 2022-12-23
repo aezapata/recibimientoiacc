536854cf9713620d5d3f9b9261706b366b3fd12c
Alexis Zapata <alexis.zapata@gm2dev.com>
2022-12-23 15:53:38 -0300

    add chakra

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,11 @@ import { Html, Main, NextScript, Head } from 'next/document'
 export default function Document() {
   return (
     <Html lang='en'>
-      <Head />
+       <Head>
+        <title>Recibimiento IACC</title>
+        <meta name="description" content="Instituto atletico central cordoba" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Main />
