69bb55166821b587c4ee75a076eff3c5f7ccc5bd
Alexis Zapata <alexis.zapata@gm2dev.com>
2022-12-23 15:35:34 -0300

    add chakra

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,10 @@
 import { ColorModeScript, theme } from '@chakra-ui/react'
-import { Html, Main, NextScript } from 'next/document'
-import Head from "next/head";
+import { Html, Main, NextScript, Head } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang='en'>
-      <Head>
-        <title>Recibimiento IACC</title>
-        <meta name="description" content="Instituto atletico central cordoba" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <Head />
       <body>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Main />
